b1fece326daf69a3172654e055c2b5af42166257
Stephen Kent <smkent@smkent.net>
2026-05-11 21:36:37 -0700

    Add craft fair display board models

---
+++ monobd/models/fair.py
@@ -0,0 +1,101 @@
+from __future__ import annotations
+
+from bdbox import Model
+from build123d import (
+    IN,
+    MM,
+    BuildPart,
+    BuildSketch,
+    Color,
+    Compound,
+    CounterSinkHole,
+    Cylinder,
+    GridLocations,
+    Locations,
+    Mode,
+    Plane,
+    Rectangle,
+    RectangleRounded,
+    chamfer,
+    extrude,
+)
+
+
+class ShelfFoot(Model):
+    diameter: float = (3 / 4) * IN
+    length: float = 4 * IN
+    thickness: float = (1 / 2) * IN
+    screw_size: float = (3 / 16) * IN
+    slop: float = (1 / 32) * IN
+
+    def build(self) -> Compound:
+        width = self.diameter + self.screw_size * 6
+        spread = width - self.screw_size * 3
+        screw_rows = 2 if self.length > self.screw_size * 8 else 1
+        with BuildPart() as p:
+            with BuildSketch():
+                RectangleRounded(self.length, width, (1 / 4) * IN)
+            extrude(amount=self.thickness)
+            Cylinder(
+                radius=(self.diameter + self.slop) / 2,
+                height=self.length,
+                rotation=(0, 90, 0),
+                mode=Mode.SUBTRACT,
+            )
+            cutout_len = self.length - self.screw_size * 6
+            if cutout_len > 0:
+                with BuildSketch():
+                    RectangleRounded(
+                        cutout_len,
+                        width - self.screw_size * 3,
+                        (1 / 4) * IN,
+                    )
+                extrude(amount=self.thickness, mode=Mode.SUBTRACT)
+            chamfer(
+                p.edges(),
+                (1 / 32 / 1.01) * IN,
+            )
+            with (
+                Locations(-Plane.XY),
+                GridLocations(
+                    self.length - self.screw_size * 3, spread, screw_rows, 2
+                ),
+            ):
+                CounterSinkHole(
+                    radius=self.screw_size / 2,
+                    counter_sink_radius=self.screw_size,
+                )
+        p.part.label = "shelf-foot"
+        p.part.color = Color(0x33CC66, alpha=0xCC)
+        return Compound(label="fair_shelf_foot", children=[p.part])
+
+
+class PictureFrame(Model):
+    length: float = 11 * IN
+    width: float = 8.5 / 2 * IN
+    frame_thick: float = 1 / 4 * IN
+    thickness: float = 5 * MM
+
+    def build(self) -> Compound:
+        with BuildPart() as p:
+            with BuildSketch():
+                RectangleRounded(
+                    self.length + self.frame_thick * 2,
+                    self.width + self.frame_thick * 2,
+                    (1 / 4) * IN,
+                )
+                Rectangle(
+                    self.length - self.frame_thick * 2,
+                    self.width - self.frame_thick * 2,
+                    mode=Mode.SUBTRACT,
+                )
+            extrude(amount=self.thickness)
+            with BuildSketch():
+                Rectangle(
+                    self.length,
+                    self.width,
+                )
+            extrude(amount=self.thickness / 2, mode=Mode.SUBTRACT)
+        p.part.label = "picture-frame"
+        p.part.color = Color(0x3399CC, alpha=0xCC)
+        return Compound(label="fair_picture_frame", children=[p.part])
